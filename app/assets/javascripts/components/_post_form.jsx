34accea9aa1ca110f048e2107b74505918c0fc93
Touqeer-tqr <tqr093@gmail.com>
2018-06-06 17:29:17 +0500

    redux packages added

--- app/assets/javascripts/components/_post_form.jsx
+++ app/assets/javascripts/components/_post_form.jsx
@@ -19,24 +19,31 @@ class PostForm extends React.Component {
     this.setState({ content: e.target.value });
   }
 
+  // is executed before rendering, on both the server and the client side.
    componentWillMount() {
       console.log('Component WILL MOUNT!')
    }
+   // is executed after the first render only on the client side.
    componentDidMount() {
       console.log('Component DID MOUNT!')
    }
+   // is invoked as soon as the props are updated before another render is called. We triggered it from setNewNumber when we updated the state.
    componentWillReceiveProps(newProps) {    
       console.log('Component WILL RECIEVE PROPS!')
    }
+   // should return true or false value. This will determine if the component will be updated or not. This is set to true by default. 
    shouldComponentUpdate(newProps, newState) {
       return true;
    }
+   // is called just before rendering.
    componentWillUpdate(nextProps, nextState) {
       console.log('Component WILL UPDATE!');
    }
+   // is called just after rendering.
    componentDidUpdate(prevProps, prevState) {
       console.log('Component DID UPDATE!')
    }
+   // is called after the component is unmounted from the dom. We are unmounting our component in main.js.
    componentWillUnmount() {
       console.log('Component WILL UNMOUNT!')
    }
